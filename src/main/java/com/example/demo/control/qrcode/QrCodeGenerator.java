e86c69fc9f42d0e24da617be34dcb58e32382900
DESKTOP-D46G95O\prezz <ge38xus@mytum.de>
2021-11-20 12:52:14 +0100

    QR Code Generator

---
+++ src/main/java/com/example/demo/control/qrcode/QrCodeGenerator.java
@@ -0,0 +1,24 @@
+package com.example.demo.control.qrcode;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
+public class QrCodeGenerator {
+    public static void createQR(String data, String path, String charset,
+                                int height, int width)
+            throws WriterException, IOException {
+
+        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset),
+                BarcodeFormat.QR_CODE, width, height);
+
+        MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
+    }
+
+}

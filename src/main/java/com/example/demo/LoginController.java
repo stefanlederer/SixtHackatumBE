8aba6a5e4eebc56c7492d300e0c0dd172a8463cf
Stefan Lederer <s.lederer@tum.de>
2021-11-20 12:56:37 +0100

    login controller and security configs

---
+++ src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/*@RestController
+public class LoginController {
+
+    @GetMapping(value={"/", "/login"})
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+}*/

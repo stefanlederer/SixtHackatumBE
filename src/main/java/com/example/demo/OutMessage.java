14f25e0641c9959a8bd9cee184efd171f58ee7f4
DESKTOP-D46G95O\prezz <ge38xus@mytum.de>
2021-11-21 09:38:13 +0100

    Chatroom works but not selfsufficient yet

---
+++ src/main/java/com/example/demo/OutMessage.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class OutMessage {
+
+    private String content;
+
+    public OutMessage() {
+    }
+
+    public OutMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
